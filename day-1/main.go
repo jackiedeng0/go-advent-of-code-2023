1b80d906c712a29623c4bcf055dbb083f6fdc4d3
Jackie Deng <jackiedeng@proton.me>
2024-03-02 12:02:39 +1100

    day-1: gofmt + defer Close()

--- day-1/main.go
+++ day-1/main.go
@@ -2,45 +2,50 @@
 package main
 
 import (
-  "fmt"
-  "log"
-  "os"
-  "bufio"
-  "strings"
-  "strconv"
-  "errors"
+	"bufio"
+	"errors"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
 )
 
 func getCValue(s string) (int, error) {
-  firstIndex := strings.IndexAny(s, "0123456789")
-  if firstIndex == -1 {
-    return 0, errors.New("No digits appear in line")
-  }
-  // lastIndex must exist if firstIndex does
-  lastIndex := strings.LastIndexAny(s, "0123456789")
-  cstr := string([]byte{s[firstIndex], s[lastIndex]})
-  cValue, _ := strconv.Atoi(cstr)
-  return cValue, nil
+	firstIndex := strings.IndexAny(s, "0123456789")
+	if firstIndex == -1 {
+		return 0, errors.New("No digits appear in line")
+	}
+	// lastIndex must exist if firstIndex does
+	lastIndex := strings.LastIndexAny(s, "0123456789")
+	cstr := string([]byte{s[firstIndex], s[lastIndex]})
+	cValue, _ := strconv.Atoi(cstr)
+	return cValue, nil
 }
 
 func main() {
+	file, err := os.Open("in.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
 
-  file, err := os.Open("in.txt")
-  if err != nil { log.Fatal(err) }
-  // Set new scanner with new lines splitter
-  reader := bufio.NewReader(file)
+	reader := bufio.NewReader(file)
 
-  sum := 0
-  for {
-    line, err := reader.ReadString('\n')
-    if err != nil { break }
+	sum := 0
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			break
+		}
 
-    cValue, err := getCValue(line)
-    if err != nil { fmt.Println(err); continue }
-    sum += cValue
-  }
+		cValue, err := getCValue(line)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		sum += cValue
+	}
 
-  fmt.Printf("Sum of calibration values: %d\n", sum)
-
+	fmt.Printf("Sum of calibration values: %d\n", sum)
 }
-
