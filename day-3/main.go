357745d26ec95eab1c06e6458a1b27f23df80eb5
Jackie Deng <jackiedeng@proton.me>
2024-03-04 20:54:28 +1100

    day-3: planning + pseudocode

---
+++ day-3/main.go
@@ -0,0 +1,25 @@
+// Day 3: Gear Ratios
+package main
+
+import (
+	"strconv"
+)
+
+// Returns
+func symbolIndicesInLine()
+
+func main() {
+	// Parse symbol locations as a row-indexed map of column-indexed maps
+	symbolsMap := map[int]map[int]bool
+	for {
+		symbolsMap[i] = symbolIndicesInLine(line)
+	}
+	// Parse numbers in line and check against symbol locations
+	sum := 0
+	for {
+		numStart, numEnd
+		// Check rows i - 1 to i + 1 from numStart - 1 to numEnd + 1
+		sum += strconv.Atoi(s)
+	}
+}
+
